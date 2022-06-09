d9b43fd2f9d41b099c34aca396704c5a1826f79a
Hector Puga <pugagarciah.@gmail.com>
2022-06-09 11:17:26 -0500

    creacion de la screen principal (Home_screen)

---
+++ lib/widgets/botones_homescreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BotonesScreen extends StatelessWidget {
+  const BotonesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
